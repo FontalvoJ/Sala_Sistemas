580e0a71516b19b67cb33909c0950ee39e381aa1
José David F <fontalvomejiajosedavid54@gmail.com>
2023-09-26 20:09:29 -0500

    Función agregar menu equipos

--- app_inventariosala.java
+++ app_inventariosala.java
@@ -1,10 +1,31 @@
 import javax.swing.JOptionPane;
 
 public class app_inventariosala {
+
+    static cls_inventario [] equipos = new cls_inventario[100];
+    static int int_posiciones = 0;
+    static boolean blm_sw;
+    static int pos;
+
     public static void main(String[] args){
         fnt_menu(true);
     }
 
+
+    private static void fnt_equipos_m1(){;
+        String codigo = JOptionPane.showInputDialog("Serial");
+        String nombre = JOptionPane.showInputDialog("Nombre");
+        String caracteristicas = JOptionPane.showInputDialog("Caracteristicas");
+        String red = JOptionPane.showInputDialog("Red");
+        String programas = JOptionPane.showInputDialog("Programas");
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Número"));
+        String ubicacion = JOptionPane.showInputDialog("Ubicacion");
+        equipos[int_posiciones] = new cls_inventario(codigo, nombre,caracteristicas, red, programas, numero, ubicacion);
+        int_posiciones += 1;
+    
+        JOptionPane.showMessageDialog(null,"Equipo registrado","Registrar", JOptionPane.INFORMATION_MESSAGE);
+
+    }
 
     private static void fnt_menu_equipos(boolean m_equipos) {
         while (m_equipos == true){
@@ -12,6 +33,9 @@ public class app_inventariosala {
             int eq =Integer.parseInt(JOptionPane.showInputDialog(null, "<<< MENU >>\n 1. Agregar\n 2.Consultar\n 3.Actualizar\n 4.Salir"));
             if (eq == 4){
                 m_equipos = false;
+            }
+            if (eq == 1){
+                fnt_equipos_m1();
             }
         }
     
